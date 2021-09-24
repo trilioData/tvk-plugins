76c8b38434e0114ec319c75967143dd8397df4ef
Pankaj Chaudhary <pankaj.chaudhary@velotio.com>
2021-09-24 13:52:09 +0530

    :seedling: TK-3964 Fixed resources args

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -56,7 +56,7 @@ helm_install() {
   echo "Installing TVK application in namespace - ${install_namespace}"
 
   common_args="applicationScope=Namespaced"
-  resources_args="backend.resources.limits.memory=1024Mi,backend.livenessProbeEnable=false,backend.resources.limits.memory=1024Mi,control-plane.resources.limits.memory=1024Mi"
+  resources_args="web-backend.resources.limits.memory=1024Mi,web-backend.livenessProbeEnable=false,web-backend.resources.requests.memory=10Mi,control-plane.resources.limits.memory=1024Mi"
   ARGS="imagePullPolicy=Always,${common_args},${resources_args}"
 
   DEV_REPO="http://charts.k8strilio.net/trilio-dev/k8s-triliovault"
