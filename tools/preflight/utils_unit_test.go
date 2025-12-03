c3c52d44b88839b2e61bdacb5ef711ba1d3b6baf
Dipayan <dipayan.pramanik@trilio.io>
2025-12-03 18:29:30 +0530

    Fixed pvc spec error in new golang version

--- tools/preflight/utils_unit_test.go
+++ tools/preflight/utils_unit_test.go
@@ -63,7 +63,7 @@ func createTestPVC(pvcKey types.NamespacedName) error {
 		Spec: corev1.PersistentVolumeClaimSpec{
 			AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
 			StorageClassName: &runOps.StorageClass,
-			Resources: corev1.ResourceRequirements{
+			Resources: corev1.VolumeResourceRequirements{
 				Requests: map[corev1.ResourceName]resource.Quantity{
 					corev1.ResourceStorage: runOps.PVCStorageRequest,
 				},
